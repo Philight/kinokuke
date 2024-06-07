b05213f95ff1eff0aec3bfd34e8f08027eb2ba96
Philight <Philight@users.noreply.github.com>
2024-06-07 15:03:27 +0200

    Create ENV.EXAMPLE.cjs

---
+++ src/data/ENV.EXAMPLE.cjs
@@ -0,0 +1,7 @@
+
+export const ENV = {
+	API_HOST: 'https://www.kkprimetech.com',
+	API_PORT: '',
+	API_POST_SEND_EMAIL: `/api/send-email`,
+	GOOGLE_API_KEY: 'AIzaSyAlFi842tZ5Wl0aikbm77AWmKKuKuu5TcA'
+}
